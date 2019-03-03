6e7cb92ff0d1fe4501ea8e3359345d225ae4f774
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2019-03-03 23:12:20 +0800

    some of the things are now organized

--- src/web/routes.py
+++ src/web/routes.py
@@ -1,11 +1,62 @@
 from web import app
 from flask import render_template, request
-import json
+import json, os
+
+from lib.dicomIO import dicomIO as dIO
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('mainApp.html')
+
+@app.route('/inpFile', methods=['GET', 'POST'])
+def inpFile():
+
+    label = '[get a folder and file]'
+    if request.method == 'POST':
+        images = request.form.getlist('dicom')
+        folder = request.form.getlist('folderName')
+
+        print('----------------------')
+        print(images)
+        print(request.form)
+        print(folder)
+        print('----------------------')
+
+        if type(images) == list:
+            fileName = images[0]
+        else:
+            fileName = images
+
+        fileName = os.path.join(folder[0], fileName)
+        print(fileName)
+        if not os.path.exists(fileName):
+            label = f'[Error: file does not exist: {fileName}'
+        else:
+            label = f'[Success: The selected file exists: {fileName}. You can proceed to select redaction list ...'
+
+            # Save the name of the folder for redaction
+            folderName = json.dumps({
+                "folderName":folder[0], 
+                "sampleFile": fileName })
+
+            with open('../config/webCache/folderName.json', 'w') as fOut:
+                fOut.write( folderName )
+
+            metaData = dIO.readFileMetaData(fileName)
+            toSave = []
+            for k in metaData:
+                toSave.append({
+                    "item": k,
+                    "checked": "",
+                    "text" : metaData[k] })
+
+            toSave = json.dumps(toSave)
+            print(toSave)
+            with open('../config/webCache/showList.json', 'w') as fOut:
+                fOut.write(toSave)
+
+    return render_template('openFile.html', label=label)
 
 @app.route('/showList', methods=['GET', 'POST'])
 def showList():
@@ -25,5 +76,23 @@ def showList():
 
 
     return render_template('showList.html', items=cachedVals)
+
+@app.route('/redact', methods=['GET', 'POST'])
+def redact():
+
+    cachedVals = json.load(open('../config/webCache/showList.json'))
+    folder     = json.load(open('../config/webCache/folderName.json'))
+    print(cachedVals)
+    print(folder)
+
+    items  = [ c['item'] for c in cachedVals if c['checked'] == 'checked']
+    others = [ c['item'] for c in cachedVals if c['checked'] != 'checked']
+    
+    if request.method == 'POST':
+        print('Redacting things ....')
+
+
+    return render_template('redact.html', folder=folder['folderName'], items = items, others=others)
+
 
 
